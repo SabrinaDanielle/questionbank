38664eb11fe7c9768ffa740a2f1ab5e8e522d016
Sabrina Danielle Green <bri.daniG@gmail.com>
2017-08-21 18:10:07 -0500

    added newquestion.pug(view), and rendered in newQuestion route

--- routes/newQuestion.js
+++ routes/newQuestion.js
@@ -1,7 +1,9 @@
 var questionRouter = require("express").Router();
 
 questionRouter.get("/", function(req, res, next) {
-  res.send("Sweet");
+  res.render("newquestion", {
+    title: "Please Add a Question & Answer to the Random Question Bank"
+  });
 });
 
 module.exports = questionRouter;
