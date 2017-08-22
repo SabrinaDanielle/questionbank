51769852835369e6a40196a7a0203b82caa4df11
Sabrina Danielle Green <bri.daniG@gmail.com>
2017-08-22 02:50:08 -0500

    Adding to database from app with timestamp

--- models/questions.model.js
+++ models/questions.model.js
@@ -1,12 +1,24 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const questionSchema = new Schema({
-  question: String,
-  answer: String,
-  author: String,
-  createAt: Date,
-  updatedAt: Date
-});
+const questionSchema = new Schema(
+  {
+    question: {
+      type: String,
+      required: true
+    },
+    answer: {
+      type: String,
+      required: true
+    },
+    author: {
+      type: String,
+      required: true
+    }
+  },
+  {
+    timestamps: true
+  }
+);
 
 module.exports = mongoose.model("bankQuestions", questionSchema);
